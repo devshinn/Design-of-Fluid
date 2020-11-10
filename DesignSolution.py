fd1880ed33b32de1175ee0ea02a0e33792ac5fca
bixbybeta <unger7586@gmail.com>
2020-11-10 10:39:40 +0900

    final

--- DesignSolution.py
+++ DesignSolution.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 import pickle
-from pylab import log10, pi, sqrt
+from pylab import *
 import random
+from matplotlib import pylab as pl
 file = open('table_data.bin','rb'); table = pickle.load(file);file.close()
 
-chen_f = lambda Ree,jdd: (-2.0*log10(jdd/3.7065 - 5.0452/Ree * log10((jdd**1.1098)/2.8257 +5.8506/(Ree**0.8981))))**-2 #Re>2100일경우
+chen_f = lambda Ree,jdd: (-2.0*pl.log10(jdd/3.7065 - 5.0452/Ree * pl.log10((jdd**1.1098)/2.8257 +5.8506/(Ree**0.8981))))**-2 #Re>2100일경우
 
-minor_loss = {'edged inlet':0.5,'Re-entrant inlet': 1, 'Well rounded inlet':0.05, 'Exit':1, '90º Elbow':0.22,'45º Elbow':0.17,'Globe valve':10,'Gate valve':0.15,'Basket strainer':1.3}
+minor_loss = {'edged inlet':0.5,'Re-entrant inlet': 1, 'Well rounded inlet':0.05, 'Exit':1, '90º Elbow':1.4,'45º Elbow':0.35,'Globe valve':10,'Gate valve':0.15,'Basket strainer':1.3}
 
 # -------------------------------- 압력차 ---------------------------------------------
 def Pressure_drop(Q, L, dz,liquid_type, pipe_standard, merterial, loss):
+
+    Q = float(Q); L = float(Q); dz = float(Q)
     k = 0
     if len(loss)==1:
-        k = int(loss[0])
+        k = float(loss[0])
     else:
         for i in range(len(loss)):
             if loss[i]== 0:
                 pass
             else:
-                k += minor_loss[loss[i][0]]*loss[i][1]
+                if loss[i][1] == '' or'0':
+                    pass
+                else:
+                    k += minor_loss[loss[i][0]]*int(loss[i][1])
 
 
     mu= table[1][liquid_type];   
@@ -39,22 +45,26 @@ def Pressure_drop(Q, L, dz,liquid_type, pipe_standard, merterial, loss):
         f = chen_f(Re,jd)
         dp = ((rho*V**2)/2)*(f*L/D + k) - dz/(rho*9.81)
         dp = dp*10**-3
-    print(f'A:{A}, V: {V}, rho{rho}, mu: {mu},  D: {D}, jd: {jd}, K: {k}, Re:{Re}, f: {f}')
-
-    return dp
+    return [dp, f]
 
 #--------------------------------유량------------------------
 
 def Flow_rate(dp, L, dz, liquid_type, pipe_standard, merterial, loss):
+    if dp=='0.000':
+        dp='0'
+    L = float(L); dz = float(dz); dp = float(dp)
     k = 0
     if len(loss)==1:
-        k = int(loss[0])
+        k = float(loss[0])
     else:
         for i in range(len(loss)):
             if loss[i]== 0:
                 pass
             else:
-                k += minor_loss[loss[i][0]]*loss[i][1]
+                if loss[i][1] == '' or'0':
+                    pass
+                else:
+                    k += minor_loss[loss[i][0]]*int(loss[i][1])
             
     mu= table[1][liquid_type];   rho=table[0][liquid_type];    D= table[2][pipe_standard];     ep= table[3][merterial];     A = (pi*D**2)/4 ;    jd = ep/D
     
@@ -64,55 +74,56 @@ def Flow_rate(dp, L, dz, liquid_type, pipe_standard, merterial, loss):
         v1 = 2*dp/rho
         v2 = 2*dz*9.81;
         v3 = (f*L/D) + k
-        V = sqrt((v1+v2)/v3)
+        V = pl.sqrt((v1+v2)/v3)
         Re = rho*V*D/mu
         f = chen_f(Re,jd)
         es = abs(af-f)
         af= f
-
-        print(V)
         Re = Re = rho*V*D/mu
     if Re <=2100: #충류일경우 f = 64/Re
         f = 64/Re
         v1 = 2*dp/rho
         v2 = 2*dz*9.81;
         v3 = (f*L/D) + k
-        V = sqrt((v1+v2)/v3)
+        V = pl.sqrt((v1+v2)/v3)
     Flow = (A*V)
-    return Flow
+    return [Flow,f]
 
 
 # --------------------------------------------------------지름----------------------------------
 def Pipe_diameter(dp,Q, L, dz, liquid_type, merterial, loss):
+    Q = float(Q); L = float(L); dz = float(dz); dp = float(dp)
     k = 0
     if len(loss)==1:
-        k = int(loss[0])
+        k = float(loss[0])
     else:
         for i in range(len(loss)):
             if loss[i]== 0:
                 pass
             else:
-                k += minor_loss[loss[i][0]]*loss[i][1]
+                if loss[i][1] == '' or'0':
+                    pass
+                else:
+                    k += minor_loss[loss[i][0]]*int(loss[i][1])
 
 
     mu= table[1][liquid_type];   rho=table[0][liquid_type];  ep= table[3][merterial];
-
-    D = random.uniform(0.001, 0.2); es = 10; af = 10
+    
+    # D = random.uniform(0.001, 0.2);
+    D = 0.0762
+    es = 10; af = 10
+  
     while es>0.0001:
-        A = (pi*D**2)/4; V=Q/A;  jd = ep/D
-        Re = rho*V*D/mu; 
+        A = (pl.pi*D**2)/4; V=Q/A;  jd = ep/D
+        
+        Re = rho*V*D/mu
         f = chen_f(Re,jd)
+        dd = f*L; a= 2*dp/(rho*V**2) ; b=19.62*dz/V**2
+        D = abs(dd/(a-b-k))
+
         es = abs(af-f)
-        af= f
-        D = (((dp/(rho*9.81) + dz)*((9.81+9.81)/V**2) - k)/(f*L))**-1
-
-    if Re <=2100: #충류일경우 f = 64/Re
-        A = (pi*D**2)/4; V=Q/A;  jd = ep/D
-        Re = rho*V*D/mu; 
-        f = 64/Re
-        D = (((dp/(rho*9.81) + dz)*((9.81+9.81)/V**2) - k)/(f*L))**-1
-
-    return D
+        af= f 
+    return [D,f]
 
 
 
--- connectUi.py
+++ connectUi.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
 __author__ = "ShinHyeok_Kim <hostomer@khu.ac.kr>"
 import sys
+import Ui
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
-from DesignSolution import*
-from time import sleep
-file = open('table_data.bin','rb'); table = pickle.load(file);file.close()
+from DesignSolution import *
 
-# Q, L, dz,dp liquid_type, pipe_standard, merterial, loss
-form_class = uic.loadUiType("DesignSolution.ui")[0]
-class WindowClass(QMainWindow, form_class) :
+
+
+
+
+class WindowClass(QMainWindow, Ui.Ui_Dialog) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+
         self. sol_arg_list = [0,0,0,0,0,0,0,0,0,0]; self.loss = [0,0,0,0,0,0,0,0,0,0]; self.summation = [0]
-        # self.syncComboBox()
+
 
         self.bt_calculation.clicked.connect(self.Bt_Calculation)
         
         #select Type
+        
         self.select_type_1.clicked.connect(lambda: self.Select_Type(self.select_type_1))
         self.select_type_2.clicked.connect(lambda: self.Select_Type(self.select_type_2))
         self.select_type_3.clicked.connect(lambda: self.Select_Type(self.select_type_3))
@@ -27,18 +28,15 @@ class WindowClass(QMainWindow, form_class) :
         #minors losses
         self.ml_check_1.stateChanged.connect(self.Minor_losses);self.ml_check_3.stateChanged.connect(self.Minor_losses);  self.ml_check_5.stateChanged.connect(self.Minor_losses);self.ml_check_7.stateChanged.connect(self.Minor_losses);
         self.ml_check_2.stateChanged.connect(self.Minor_losses);self.ml_check_4.stateChanged.connect(self.Minor_losses);  self.ml_check_6.stateChanged.connect(self.Minor_losses);self.ml_check_8.stateChanged.connect(self.Minor_losses);
-        self.ml_check_9.stateChanged.connect(self.Minor_losses);self.ml_check_10.stateChanged.connect(self.Minor_losses);
+        self.ml_check_9.stateChanged.connect(self.Minor_losses);self.ml_check_10.stateChanged.connect(self.Sum_K);
         # minors losses 갯수
-        # minors losses 갯수
-        # minors losses 갯수
+
         self.ml_num_1.valueChanged.connect(self.Minor_losses);self.ml_num_3.valueChanged.connect(self.Minor_losses);self.ml_num_5.valueChanged.connect(self.Minor_losses);self.ml_num_7.valueChanged.connect(self.Minor_losses)
         self.ml_num_2.valueChanged.connect(self.Minor_losses);self.ml_num_4.valueChanged.connect(self.Minor_losses);self.ml_num_6.valueChanged.connect(self.Minor_losses);self.ml_num_8.valueChanged.connect(self.Minor_losses)
         self.ml_num_9.valueChanged.connect(self.Minor_losses)
 
         self.pipe_length_value.valueChanged.connect(self.Minor_losses);self.height_drop_value.valueChanged.connect(self.Minor_losses);
-        self.flow_rate_value.valueChanged.connect(self.Minor_losses);self.pressure_drop_value.valueChanged.connect(self.Minor_losses)
-
-#new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new
+        self.flow_rate_value.valueChanged.connect(self.Minor_losses);self.pressure_drop_value.valueChanged.connect(self.Minor_losses); self.sum_k.valueChanged.connect(self.Sum_K)
         
         self.calcul_list.itemDoubleClicked.connect(self.DoubleClice_remove_list_item)
         # self.calcul_list.currentItemChanged.connect(self.chkCurrentItemChanged)
@@ -49,86 +47,90 @@ class WindowClass(QMainWindow, form_class) :
         self.pm_add_btn.clicked.connect(self.pm_addListWidget)
         self.bt_clear.clicked.connect(self.clear_Btn) # clear button
 
+    def clear_Btn(self) : # clear function
+        self.calcul_list.clear()
+        self.calcul_list.addItem('선택목록')
+        #self.selec_type_line.clear(); self.selec_type_line.setText('Type : ')
+        self.pipe_length_value.clear()
+        self.sol_arg_list = [0,0,0,0,0,0,0,0,0,0]; self.loss = [0,0,0,0,0,0,0,0,0]
+        self.sol_arg_list[0] = self.selec_type_line.text()
+        self.ml_num_1.clear()
+        self.sum_k.clear
+        self.err_message.setText('')
+
 #더블클릭 삭제
     def DoubleClice_remove_list_item(self) : 
 
         self.removeItemRow = self.calcul_list.currentRow()
         self.calcul_list.takeItem(self.removeItemRow)
 
-
+        self.err_message.setText('')
  # 콤보 박스에서 리스트위젯으로
     def lt_addListWidget(self) :
         self.addItemText = self.lt_combo.currentText()
         self.calcul_list.addItem(self.addItemText)
         self.sol_arg_list[1]=self.lt_combo.currentText()
-
+        self.err_message.setText('')
     def pm_addListWidget(self):
         self.addItemText = self.pm_combo.currentText()
         self.calcul_list.addItem(self.addItemText)
         self.sol_arg_list[3]=self.pm_combo.currentText()
-
+        self.err_message.setText('')
 
     def ps_addListWidget(self):
         self.addItemText = self.ps_combo.currentText()
         self.calcul_list.addItem(self.addItemText)
         self.sol_arg_list[2]=self.ps_combo.currentText()
 
+        self.err_message.setText('')
 
-    def clear_Btn(self) : # clear function
-        self.calcul_list.clear()
-        self.calcul_list.addItem('선택목록')
-        #self.selec_type_line.clear(); self.selec_type_line.setText('Type : ')
-        self.pipe_length_value.clear()
-        self. sol_arg_list = [0,0,0,0,0,0,0,0,0,0]; self.loss = [0,0,0,0,0,0,0,0]
         
 
     def Select_Type(self, st):
         if st.isChecked(): 
             self.sol_arg_list[0]=st.text()
             self.selec_type_line.setText('Type: '+ self.sol_arg_list[0])
- 
+        self.err_message.setText('')
 
     def Sum_K(self):
-        if self.ml_check_10.isChecked(): self.summation[0]=[self.sum_k.text()]
-
+        if self.ml_check_10.isChecked():
+            self.summation[0]=self.sum_k.text()
+            self.err_message.setText('')
     def Minor_losses(self):
-        self.sol_arg_list[4]=self.flow_rate_value.value() # Q
-        self.sol_arg_list[5]=self.pipe_length_value.value() # L
-        self.sol_arg_list[6]=self.height_drop_value.value() # dz
-        self.sol_arg_list[7]=self.pressure_drop_value.value()#dp 
+        self.sol_arg_list[4]=self.flow_rate_value.text() # Q
+        self.sol_arg_list[5]=self.pipe_length_value.text() # L
+        self.sol_arg_list[6]=self.height_drop_value.text()# dz
+        self.sol_arg_list[7]=self.pressure_drop_value.text()#dp 
         
-        if self.ml_check_1.isChecked(): self.loss[0]=[self.ml_check_1.text(),self.ml_num_1.value()]
-        if self.ml_check_2.isChecked(): self.loss[1]=[self.ml_check_2.text(),self.ml_num_2.value()]
-        if self.ml_check_3.isChecked(): self.loss[2]=[self.ml_check_3.text(),self.ml_num_3.value()]
-        if self.ml_check_4.isChecked(): self.loss[3]=[self.ml_check_4.text(),self.ml_num_4.value()]
-        if self.ml_check_5.isChecked(): self.loss[4]=[self.ml_check_5.text(),self.ml_num_5.value()]
-        if self.ml_check_6.isChecked(): self.loss[5]=[self.ml_check_6.text(),self.ml_num_6.value()]
-        if self.ml_check_7.isChecked(): self.loss[6]=[self.ml_check_7.text(),self.ml_num_7.value()]
-        if self.ml_check_8.isChecked(): self.loss[7]=[self.ml_check_8.text(),self.ml_num_8.value()]
-        if self.ml_check_9.isChecked(): self.loss[8]=[self.ml_check_9.text(),self.ml_num_9.value()]
-   
+        if self.ml_check_1.isChecked(): self.loss[0]=[self.ml_check_1.text(),self.ml_num_1.text()]
+        if self.ml_check_2.isChecked(): self.loss[1]=[self.ml_check_2.text(),self.ml_num_2.text()]
+        if self.ml_check_3.isChecked(): self.loss[2]=[self.ml_check_3.text(),self.ml_num_3.text()]
+        if self.ml_check_4.isChecked(): self.loss[3]=[self.ml_check_4.text(),self.ml_num_4.text()]
+        if self.ml_check_5.isChecked(): self.loss[4]=[self.ml_check_5.text(),self.ml_num_5.text()]
+        if self.ml_check_6.isChecked(): self.loss[5]=[self.ml_check_6.text(),self.ml_num_6.text()]
+        if self.ml_check_7.isChecked(): self.loss[6]=[self.ml_check_7.text(),self.ml_num_7.text()]
+        if self.ml_check_8.isChecked(): self.loss[7]=[self.ml_check_8.text(),self.ml_num_8.text()]
+        if self.ml_check_9.isChecked(): self.loss[8]=[self.ml_check_9.text(),self.ml_num_9.text()]
+        self.err_message.setText('')
     
     def Bt_Calculation(self):
-        print(self.sol_arg_list)
-       
         if self.summation[0] != 0:
             self.loss = self.summation
-        print(self.loss)
         try:
-            self.flow_rate_value.clear()
             if self.sol_arg_list[0] == 'Pressure Drop':
                 result = Pressure_drop(self.sol_arg_list[4], self.sol_arg_list[5], self.sol_arg_list[6], self.sol_arg_list[1], self.sol_arg_list[2], self.sol_arg_list[3], self.loss) # Q, L, dz,liquid_type, pipe_standard, merterial, self.loss
-                self.solution = str(round(result,3))+' KPa'
-            elif self.self.sol_arg_list[0] == 'Flow Rate':
-                result = Flow_rate(self.self.sol_arg_list[7], self.sol_arg_list[5], self.sol_arg_list[6], self.sol_arg_list[1],self.sol_arg_list[2], self.sol_arg_list[3], self.loss) #dp, L, dz, liquid_type, pipe_standard, merterial, self.loss
-                self.solution = str(result)+' m³/s'
-            elif self.self.sol_arg_list[0] == 'Diameter':
-                result = Pipe_diameter(self.self.sol_arg_list[7],self.sol_arg_list[4], self.sol_arg_list[5], self.sol_arg_list[6], self.sol_arg_list[1], self.sol_arg_list[3], self.loss)
-                self.solution = str(round(result,5))+' m'
+                self.solution = str(round(result[0],4))+' KPa'+ '    f:'+str(round(result[1],4))
+            elif self.sol_arg_list[0] == 'Flow Rate':
+                result = Flow_rate(self.sol_arg_list[7], self.sol_arg_list[5], self.sol_arg_list[6], self.sol_arg_list[1],self.sol_arg_list[2], self.sol_arg_list[3], self.loss) #dp, L, dz, liquid_type, pipe_standard, merterial, self.loss
+                self.solution = str(round(result[0],4))+' m³/s'+'  f: '+str(round(result[1],4))
+            elif self.sol_arg_list[0] == 'Diameter':
+                result = Pipe_diameter(self.sol_arg_list[7],self.sol_arg_list[4], self.sol_arg_list[5], self.sol_arg_list[6], self.sol_arg_list[1], self.sol_arg_list[3], self.loss)
+                self.solution = str(round(result,5))+' m'+'   f: '+str(round(result[1],4))
+                
             self.result_line.setText(self.solution) #str(result)
         
-        except (AttributeError,KeyError):
-            self.err_message.setText('필수 요소 를 확인해 주세요!!')
+        except (AttributeError,KeyError,ZeroDivisionError):
+            self.err_message.setText('필수 요소 를 확인 or 재시작!!')
             pass
             
 if __name__ == "__main__" :
@@ -136,3 +138,4 @@ if __name__ == "__main__" :
     myWindow = WindowClass()
     myWindow.show()
     app.exec_()
+
